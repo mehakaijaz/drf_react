626ee9d2211f27e67ab9fd58b2c2df5948b7bb67
mehak aijaz <mehakaijaz716@gmail.com>
2024-08-14 17:20:07 +0530

    setting up template

---
+++ frontend/ecom/src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/screens/Header';
+import Footer from './components/screens/Footer';
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
