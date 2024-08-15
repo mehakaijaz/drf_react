0633b8339d78bfe2b125035991202453af13b6d0
mehak aijaz <mehakaijaz716@gmail.com>
2024-08-15 20:45:46 +0530

    changes in components

--- frontend/ecom/src/App.js
+++ frontend/ecom/src/App.js
@@ -1,11 +1,27 @@
-import Header from './components/screens/Header';
-import Footer from './components/screens/Footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import HomeScreen from './components/screens/HomeScreen';
+import LoginScreen from './components/screens/LoginScreen';
+import SignupScreen from './components/screens/SignupScreen';
+import CartScreen from './components/screens/CartScreen';
 
 function App() {
   return (
     <>
-      <Footer /></>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path='/' element={<HomeScreen />}></Route></Routes>
+        <Routes>
+          <Route exact path='/login' element={<LoginScreen />}></Route></Routes>
+        <Routes>
+          <Route exact path='/signup' element={<SignupScreen />}></Route></Routes>
+        <Routes>
+          <Route exact path='/cart' element={<CartScreen />}></Route></Routes>
+
+      </Router></>
   );
 }
 
